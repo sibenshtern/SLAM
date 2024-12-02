fix: quaternions, refactor code

visualization/scripts/align.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.spatial.transform import Rotation as R
 
 
-def align_timestamps(timestamps_gt, positions_gt, quaternions_gt, timestamps_a, flag_ts=True):
+def align_timestamps(timestamps_gt, positions_gt, quaternions_gt, timestamps_a):
     """
     align parameters of groundtruth trajectory with algorithm timestamps
 
@@ -12,7 +12,7 @@ def align_timestamps(timestamps_gt, positions_gt, quaternions_gt, timestamps_a, 
         parameters from groundtruth
     timestamps_a : numpy.ndarray
         timestamps from algorithm
-    
+
     Returns
     -------
     aligned_positions_gt, aligned_quaternions_gt : numpy.ndarray
@@ -37,13 +37,17 @@ def align_timestamps(timestamps_gt, positions_gt, quaternions_gt, timestamps_a, 
     aligned_positions_gt = np.array(aligned_positions_gt)
     aligned_quaternions_gt = np.array(aligned_quaternions_gt)
 
-    if flag_ts:
-        return aligned_timestamps_gt, aligned_positions_gt, aligned_quaternions_gt
-    else:
-        return aligned_positions_gt, aligned_quaternions_gt
+    return aligned_timestamps_gt, aligned_positions_gt, aligned_quaternions_gt
 
 
-def align_trajectories(timestamps_gt, positions_gt, quaternions_gt, timestamps_a, positions_a, quaternions_a):
+def align_trajectories(
+    timestamps_gt,
+    positions_gt,
+    quaternions_gt,
+    timestamps_a,
+    positions_a,
+    quaternions_a,
+):
     """
     align algorithm trajectory with groundtruth trajectory
 
@@ -53,13 +57,15 @@ def align_trajectories(timestamps_gt, positions_gt, quaternions_gt, timestamps_a
         parameters from groundtruth
     timestamps_a, positions_a, quaternions_a : numpy.ndarray
         parameters from algorithm
-    
+
     Returns
     -------
     aligned_positions_a, aligned_quaternions_a : numpy.ndarray
         aligned parameters of algorithm trajectory
     """
-    timestamps_gt, positions_gt, quaternions_gt = align_timestamps(timestamps_gt, positions_gt, quaternions_gt, timestamps_a)
+    timestamps_gt, positions_gt, quaternions_gt = align_timestamps(
+        timestamps_gt, positions_gt, quaternions_gt, timestamps_a
+    )
 
     mu_gt = np.mean(positions_gt, axis=0)
     mu_a = np.mean(positions_a, axis=0)
@@ -70,28 +76,22 @@ def align_trajectories(timestamps_gt, positions_gt, quaternions_gt, timestamps_a
     n = np.shape(positions_gt)[0]
 
     # cross-covariance matrix
-    C = 1.0 / n * np.dot(centered_gt.T, centered_a)
-    sigma2 = 1.0 / n * np.multiply(centered_a, centered_a).sum()
+    C = np.dot(centered_gt.T, centered_a) / n
 
-    U, D, Vt = np.linalg.linalg.svd(C)
-    D = np.diag(D)
+    U, _, Vt = np.linalg.linalg.svd(C)
 
     W = np.eye(3)
-    if(np.linalg.det(U) * np.linalg.det(Vt.T) < 0):
+    if np.linalg.det(U) * np.linalg.det(Vt.T) < 0:
         W[2, 2] = -1
 
     delta_R = np.dot(U, np.dot(W, Vt))
-    s = 1.0 / sigma2 * np.trace(np.dot(D, W))
-    
-    # in SE(3):
-    s = 1
 
-    delta_t = mu_gt - s * np.dot(delta_R, mu_a)
+    delta_t = mu_gt - np.dot(delta_R, mu_a)
 
-    aligned_positions_a = s * np.dot(delta_R, positions_a.T).T + delta_t
+    aligned_positions_a = np.dot(delta_R, positions_a.T).T + delta_t
 
     aligned_quaternions_a = []
-    for (q_gt, q_a) in [*zip(quaternions_gt, quaternions_a)]:
+    for q_gt, q_a in [*zip(quaternions_gt, quaternions_a)]:
         R_a = R.from_quat(q_a).as_matrix()
         R_aligned = np.dot(delta_R, R_a)
         q_aligned = R.from_matrix(R_aligned).as_quat()

visualization/scripts/errors.py
@@ -4,7 +4,14 @@ from scipy.spatial.transform import Rotation as R
 import align
 
 
-def compute_ate(timestamps_gt, positions_gt, quaternions_gt, timestamps_a, positions_a, quaternions_a):
+def compute_ate(
+    timestamps_gt,
+    positions_gt,
+    quaternions_gt,
+    timestamps_a,
+    positions_a,
+    quaternions_a,
+):
     """
     compute absolute trajectory error (ATE) for positions and rotations
 
@@ -14,7 +21,7 @@ def compute_ate(timestamps_gt, positions_gt, quaternions_gt, timestamps_a, posit
         parameters from groundtruth
     timestamps_a, positions_a, quaternions_a : numpy.ndarray
         parameters from algorithm
-    
+
     Returns
     -------
     ate_pos : float
@@ -25,11 +32,15 @@ def compute_ate(timestamps_gt, positions_gt, quaternions_gt, timestamps_a, posit
     ate_pos = []
     ate_rot = []
 
-    trajectories_gt_aligned, positions_gt_aligned, quaternions_gt_aligned = align.align_timestamps(timestamps_gt, positions_gt, quaternions_gt, timestamps_a)
-    
+    trajectories_gt_aligned, positions_gt_aligned, quaternions_gt_aligned = (
+        align.align_timestamps(
+            timestamps_gt, positions_gt, quaternions_gt, timestamps_a
+        )
+    )
+
     # position error
     delta_pos = positions_gt_aligned - positions_a
-    ate_pos = np.sqrt(np.mean(np.sum(delta_pos ** 2, axis=1)))
+    ate_pos = np.sqrt(np.mean(np.sum(delta_pos**2, axis=1)))
 
     # rotation error
     delta_rot = [
@@ -45,6 +56,7 @@ def compute_cumulative_distances(positions):
     """compute cumulative distance of trajectory from positions"""
     deltas = np.linalg.norm(np.diff(positions, axis=0), axis=1)
     return np.insert(np.cumsum(deltas), 0, 0)
+
 
 def get_subtraj(dist, delta):
     """find subtrajectories with a given length (offset<0.5)"""
@@ -66,7 +78,15 @@ def get_subtraj(dist, delta):
     return pairs
 
 
-def compute_rpe(timestamps_gt, positions_gt, quaternions_gt, timestamps_a, positions_a, quaternions_a, distances):
+def compute_rpe(
+    timestamps_gt,
+    positions_gt,
+    quaternions_gt,
+    timestamps_a,
+    positions_a,
+    quaternions_a,
+    distances,
+):
     """
     compute relative pose error (RPE) for positions and rotations
 
@@ -89,8 +109,12 @@ def compute_rpe(timestamps_gt, positions_gt, quaternions_gt, timestamps_a, posit
     rpe_pos = []
     rpe_rot = []
 
-    trajectories_gt_aligned, positions_gt_aligned, quaternions_gt_aligned = align.align_timestamps(timestamps_gt, positions_gt, quaternions_gt, timestamps_a)
-    
+    trajectories_gt_aligned, positions_gt_aligned, quaternions_gt_aligned = (
+        align.align_timestamps(
+            timestamps_gt, positions_gt, quaternions_gt, timestamps_a
+        )
+    )
+
     dist_gt = compute_cumulative_distances(positions_gt_aligned)
     dist_a = compute_cumulative_distances(positions_a)
 
@@ -105,26 +129,38 @@ def compute_rpe(timestamps_gt, positions_gt, quaternions_gt, timestamps_a, posit
             delta_ts_gt = trajectories_gt_aligned[start_idx:end_idx]
             delta_pos_gt = positions_gt_aligned[start_idx:end_idx]
             delta_quat_gt = quaternions_gt_aligned[start_idx:end_idx]
-            
+
             # select algorithm subtrajectory
             delta_ts_a = timestamps_a[start_idx:end_idx]
             delta_pos_a = positions_a[start_idx:end_idx]
             delta_quat_a = quaternions_a[start_idx:end_idx]
-            
+
             # align algorithm subtrajectory to gt subtrajectory
-            delta_pos_a, delta_quat_a = align. align_trajectories(delta_ts_gt, delta_pos_gt, delta_quat_gt, delta_ts_a, delta_pos_a, delta_quat_a)
+            delta_pos_a, delta_quat_a = align.align_trajectories(
+                delta_ts_gt,
+                delta_pos_gt,
+                delta_quat_gt,
+                delta_ts_a,
+                delta_pos_a,
+                delta_quat_a,
+            )
 
             # calculate rpe position
-            delta_pos_gt = positions_gt_aligned[end_idx] - positions_gt_aligned[start_idx]
+            delta_pos_gt = (
+                positions_gt_aligned[end_idx] - positions_gt_aligned[start_idx]
+            )
             delta_pos_a = positions_a[-1] - positions_a[0]
             rpe_pos_d.append(np.linalg.norm(delta_pos_gt - delta_pos_a))
 
             # calculate rpe rotation
-            rel_rot_gt = R.from_quat(quaternions_gt_aligned[start_idx]).inv() * R.from_quat(quaternions_gt_aligned[end_idx])
-            rel_rot_a = R.from_quat(delta_quat_a[0]).inv() * R.from_quat(delta_quat_a[-1])
+            rel_rot_gt = R.from_quat(
+                quaternions_gt_aligned[start_idx]
+            ).inv() * R.from_quat(quaternions_gt_aligned[end_idx])
+            rel_rot_a = R.from_quat(delta_quat_a[0]).inv() * R.from_quat(
+                delta_quat_a[-1]
+            )
             delta_rot = rel_rot_gt.inv() * rel_rot_a
             rpe_rot_d.append(delta_rot.magnitude())
-
 
         rpe_pos[d - 1] = np.mean(np.mean(rpe_pos_d))
         rpe_rot[d - 1] = np.mean(np.mean(rpe_rot_d))

visualization/scripts/plot.py
@@ -9,14 +9,13 @@ import load
 import align
 
 
-def draw_trajectory(positions, quaternions, frequency=30, color=[1, 0, 0]):
+def draw_trajectory(positions, quaternions, color=[1, 0, 0]):
     """
     draw single trajectory using open3d
 
     Parameters
     ----------
     positions, quaternions : numpy.ndarray
-    frequency : int, default=30
     color : list, default=[1, 0, 0]
 
     Returns
@@ -25,17 +24,15 @@ def draw_trajectory(positions, quaternions, frequency=30, color=[1, 0, 0]):
     """
     frames = []
 
-    for i, (pos, quat) in enumerate(zip(positions[::frequency], quaternions[::frequency])):
-        #f = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.2)
-
-        size = 0.2
+    for i, (pos, quat) in enumerate(zip(positions, quaternions)):
+        size = 0.08
 
         np_points = np.array([[0, 0, 0], [size, 0, 0], [0, size, 0], [0, 0, size]])
         np_lines = np.array([[0, 1], [0, 2], [0, 3]])
 
         l = o3d.geometry.LineSet(
             points=o3d.utility.Vector3dVector(np_points),
-            lines=o3d.utility.Vector2iVector(np_lines)
+            lines=o3d.utility.Vector2iVector(np_lines),
         )
 
         rot = rot = R.from_quat(quat).as_matrix()
@@ -48,6 +45,7 @@ def draw_trajectory(positions, quaternions, frequency=30, color=[1, 0, 0]):
 
     return frames
 
+
 def plot_from_filepaths(args):
     """
     plot (single / multiple) trajectories from filepaths
@@ -55,47 +53,75 @@ def plot_from_filepaths(args):
     Parameters
     ----------
     filepath : str
-    frequency : int, default=30
     """
     frames = []
-    colors = [colorsys.hsv_to_rgb(i / len(args.filepaths), 1, 1) for i in range(len(args.filepaths))]
+    colors = [
+        colorsys.hsv_to_rgb(i / len(args.filepaths), 1, 1)
+        for i in range(len(args.filepaths))
+    ]
 
     # gt
     t_gt, p_gt, q_gt = load.load_trajectory(args.filepaths[0])
-    frames += draw_trajectory(p_gt, q_gt, args.frequency, colors[-1])
-    
+
+    # qw, qx, qy, qz -> qx, qy, qz, qw
+    q_gt = q_gt[:, [1, 2, 3, 0]]
+
+    timestamps = [t_gt]
+    positions = [p_gt]
+    quaternions = [q_gt]
+
     for i, f in enumerate(args.filepaths[1:]):
-        timestamps, positions, quaternions = load.load_trajectory(f)
-        positions, quaternions = align.align_trajectories(t_gt, p_gt, q_gt, timestamps, positions, quaternions)
-        frames += draw_trajectory(positions, quaternions, args.frequency, colors[i])
+        t_a, p_a, q_a = load.load_trajectory(f)
+        p_a, q_a = align.align_trajectories(t_gt, p_gt, q_gt, t_a, p_a, q_a)
+        timestamps.append(t_a)
+        positions.append(p_a)
+        quaternions.append(q_a)
+
+    # find shortest trajectory
+    idx_min = np.argmin([len(ts) for ts in timestamps])
+
+    for i in range(len(args.filepaths)):
+        ts, p, q = align.align_timestamps(
+            timestamps[i], positions[i], quaternions[i], timestamps[idx_min]
+        )
+        frames += draw_trajectory(p, q, colors[i])
 
     o3d.visualization.draw_geometries(frames)
 
 
-def plot_from_pq(args):
+def plot_from_tpq(args):
     """
-    plot (single / multiple) trajectories from positions and quaternions
+    plot (single / multiple) trajectories from timestamps, positions and quaternions
 
     Parameters
     ----------
-    positions, quaternions : list
-    frequency : int, default=30
+    timestamps, positions, quaternions : numpy.ndarray
     """
     frames = []
-    colors = [colorsys.hsv_to_rgb(i / len(args.positions), 1, 1) for i in range(len(args.positions))]
+    colors = [
+        colorsys.hsv_to_rgb(i / len(args.positions), 1, 1)
+        for i in range(len(args.timestamps))
+    ]
 
-    for i, (pos, quat) in enumerate([*zip(args.positions, args.quaternions)]):
-        frames += draw_trajectory(pos, quat, args.frequency, colors[i])
+    # find shortest trajectory
+    idx_min = np.argmin([len(ts) for ts in args.timestamps])
+
+    for i in range(len(args.timestamps)):
+        ts, p, q = align.align_timestamps(
+            args.timestamps[i],
+            args.positions[i],
+            args.quaternions[i],
+            args.timestamps[idx_min],
+        )
+        frames += draw_trajectory(p, q, colors[i])
 
     o3d.visualization.draw_geometries(frames)
-    
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="plot.py")
 
     parser.add_argument("filepaths", nargs="+", help="paths to the trajectories")
-    parser.add_argument("-f", "--frequency", nargs='?', type=int, default=30,
-                        help="frequency of plotted trajectories")
 
     args = parser.parse_args()
 